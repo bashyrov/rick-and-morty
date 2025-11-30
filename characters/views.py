79ae954177a9a69c6a83a3f75dade760ff1be73e
Artur Bashyrov <bashyroov@gmail.com>
2025-11-30 20:56:50 +0100

    Added swagger documentation using drf-spectacular

--- characters/views.py
+++ characters/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -9,8 +10,10 @@ from characters.models import Character
 from characters.serializers import CharacterSerializer
 
 
+@extend_schema(responses={status.HTTP_200_OK: CharacterSerializer})
 @api_view(["GET"])
 def get_random_character_view(request: Request) -> Response:
+    """Get random character from Rick and Morty World"""
     pks = Character.objects.values_list("pk", flat=True)
     random_pk = choice(pks)
     random_character = Character.objects.get(pk=random_pk)
@@ -31,3 +34,17 @@ class CharacterListView(generics.ListAPIView):
             qs = qs.filter(name__icontains=name)
 
         return qs
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="name",
+                description="Filter by name insensitive contains",
+                required=False,
+                type=str,
+            ),
+        ]
+    )
+    def get(self, request, *args, **kwargs) -> Response:
+        """List characters with filtering by name."""
+        return self.list(request, *args, **kwargs)
